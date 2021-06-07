7d80f637cfc2daf84e25a281e9c6c461ceb95168
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-07 19:39:43 -0300

    search feature added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,9 +20,22 @@ class App extends Component {
   }
 
   render() {
+
+    //filter the monsters
+    const { monsters, searchField } = this.state;
+    const filter_monsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input type='search'
+          placeholder='search for players'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+
+
+        <CardList monsters={filter_monsters} />
+
       </div>
     );
   }
